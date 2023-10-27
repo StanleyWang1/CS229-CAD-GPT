89016f0ba61c9414f2db662962768a9b5706486b
StanleyWang1 <swang11@stanford.edu>
2023-10-26 18:24:47 -0700

    new 600x McMaster dataset

--- MCMASTER_DATA/NBW_classifier/ANALYSIS.py
+++ MCMASTER_DATA/NBW_classifier/ANALYSIS.py
@@ -1,60 +1,63 @@
-import csv
+import util
+import numpy as np
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from sklearn.preprocessing import StandardScaler
 
-filename = './MCMASTER_DATA/NEW_classifier/'
+import matplotlib.pyplot as plt
 
-def add_intercept(x):
-    """Add intercept to matrix x.
+training_path = './MCMASTER_DATA/training_x_300.csv'
+[x_train, y_train] = util.load_dataset(training_path, label_col='y', add_intercept=True)
 
-    Args:
-        x: 2D NumPy array.
+# Feature Engineering
+f1 = np.divide(x_train[:,0], x_train[:,5]) # V / BBV ratio
+f2 = np.divide(x_train[:,2], x_train[:,4]) # X / Z ratio
+f3 = np.divide(x_train[:,3], x_train[:,4]) # Y / Z ratio
+f4 = np.divide(x_train[:,6], x_train[:,7]) # NV / NE ratio
+f5 = np.divide(x_train[:,6], x_train[:,8]) # NV / NF ratio
+f6 = np.divide(x_train[:,7], x_train[:,8]) # NE / NF ratio
+x_train2 = np.vstack((f1, f2, f3, f4, f5, f6)).T
+x_train3 = np.concatenate((x_train, x_train2), axis=1)
 
-    Returns:
-        New matrix same as x with 1's in the 0th column.
-    """
-    new_x = np.zeros((x.shape[0], x.shape[1] + 1), dtype=x.dtype)
-    new_x[:, 0] = 1
-    new_x[:, 1:] = x
+BBdims = np.vstack((x_train[:,2], x_train[:,3], x_train[:,4])).T         
+max_values = np.max(BBdims, axis=1)
+max_values = max_values.reshape(-1, 1)
+min_values = np.min(BBdims, axis=1)
+min_values = min_values.reshape(-1, 1)
+aspect_ratio = np.divide(min_values, max_values)
 
-    return new_x
+packing_density = np.divide(x_train[:,0], x_train[:,5])
+packing_density = np.reshape(packing_density, (300, 1))
 
+x_train2D = np.concatenate((aspect_ratio[y_train != 2], packing_density[y_train != 2]), axis=1)
+y_train2D = y_train[y_train != 2]
+# x_train2D = np.concatenate((min_values[y_train != 2], max_values[y_train != 2]), axis=1)
+# y_train2D = y_train[y_train != 2]
 
-def load_dataset(csv_path, label_col='y', add_intercept=False):
-    """Load dataset from a CSV file.
+print(np.shape(x_train2D))
+print(np.shape(y_train2D))
+# # scaler = StandardScaler().fit(x_train3)
+# # x_scaled = scaler.transform(x_train3)
 
-    Args:
-         csv_path: Path to CSV file containing dataset.
-         label_col: Name of column to use as labels (should be 'y' or 't').
-         add_intercept: Add an intercept entry to x-values.
+model = LogisticRegression(solver='lbfgs', multi_class='multinomial', max_iter=1000)
+model.fit(x_train2D, y_train2D)
+y_pred = model.predict(x_train2D)
+accuracy = accuracy_score(y_train2D, y_pred)
+print(f"Accuracy: {accuracy}")
 
-    Returns:
-        xs: Numpy array of x-values (inputs).
-        ys: Numpy array of y-values (labels).
-    """
+# plt.figure()
+# plt.scatter(np.reshape(x_train2D[:,0], (200, 1)), np.reshape(x_train2D[:,1], (200, 1)))
+# plt.show()
 
-    def add_intercept_fn(x):
-        global add_intercept
-        return add_intercept(x)
+print(np.shape(x_train2D[y_train2D==0][:, 0]))
+print(np.shape(x_train2D[y_train2D==0][:, 1]))
 
-    # Validate label_col argument
-    allowed_label_cols = ('y', 't')
-    if label_col not in allowed_label_cols:
-        raise ValueError('Invalid label_col: {} (expected {})'
-                         .format(label_col, allowed_label_cols))
+plt.figure()
+plt.scatter(x_train2D[y_train2D==0][:, 0], x_train2D[y_train2D==0][:, 1], color='blue')
+plt.scatter(x_train2D[y_train2D==1][:, 0], x_train2D[y_train2D==1][:, 1], color='red')
+plt.xlabel('Aspect Ratio')
+plt.ylabel('Packing Density')
+plt.show()
 
-    # Load headers
-    with open(csv_path, 'r') as csv_fh:
-        headers = csv_fh.readline().strip().split(',')
-
-    # Load features and labels
-    x_cols = [i for i in range(len(headers)) if headers[i].startswith('x')]
-    l_cols = [i for i in range(len(headers)) if headers[i] == label_col]
-    inputs = np.loadtxt(csv_path, delimiter=',', skiprows=1, usecols=x_cols)
-    labels = np.loadtxt(csv_path, delimiter=',', skiprows=1, usecols=l_cols)
-
-    if inputs.ndim == 1:
-        inputs = np.expand_dims(inputs, -1)
-
-    if add_intercept:
-        inputs = add_intercept_fn(inputs)
-
-    return inputs, labels
+# util.plot_decision_boundary(model, x_train2D, y_train2D, feature_names=['Aspect Ratio', 'Packing Density'])
